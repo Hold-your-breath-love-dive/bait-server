99eaf9a7d41e4caef15b6dca358f05af88328328
Mercen <mercen@mercen.net>
2023-07-12 17:49:36 +0900

    ADD: Some Functions for Controller

--- Sources/App/Controllers/CommentController.swift
+++ Sources/App/Controllers/CommentController.swift
@@ -4,15 +4,30 @@ import Vapor
 struct CommentController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let comment = routes.grouped("comment")
-        comment.get("list", use: getList)
+        comment.get(":parentId", use: getList)
         comment.post(use: post)
-        comment(":id").delete(use: delete)
+        comment.delete(":id", use: delete)
     }
     
     func getList(req: Request) async throws -> [GetComment] {
-        try await Comment.query(on: req.db)
+        guard let id = req.parameters.get("parentId"),
+              let idInt = Int(id)
+        else {
+            throw Abort(.badRequest)
+        }
+        return try await Comment
+            .query(on: req.db)
+            .filter(\.$parentId == idInt)
             .sort(\.$createDate, .ascending)
             .all()
             .map { $0.toDTO() }
+    }
+    
+    func post(req: Request) async throws -> HTTPStatus {
+        
+    }
+    
+    func delete(req: Request) async throws -> HTTPStatus {
+        
     }
 }
