bf7d4232722b9ccc85b62bb97701c109036fe2b2
Mercen <mercen@mercen.net>
2023-07-12 16:45:12 +0900

    ADD: Comment Feature

---
+++ Sources/App/Controllers/CommentController.swift
@@ -0,0 +1,12 @@
+struct CommentController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        let writing = routes.grouped("writing")
+        writing.get("list", use: getList)
+        writing.post(use: post)
+        writing.group(":id") { writingID in
+            writingID.get(use: getOne)
+            writingID.put(use: put)
+            writingID.delete(use: delete)
+        }
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,5 +2,6 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+    try app.register(collection: CommentController())
     try app.register(collection: WritingController())
 }
